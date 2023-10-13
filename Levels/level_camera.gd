43742a6a5c7dbdc4211cc1b06125d829056a5aa4
karvalho <renato@karvalho.com.br>
2023-10-13 14:04:23 -0300

    Improved camera following

--- Levels/level_camera.gd
+++ Levels/level_camera.gd
@@ -5,14 +5,12 @@ class_name LevelCamera extends Node3D
 @onready var camera: Camera3D = %Camera3D
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player.camera_controller = camera
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if not player:
 		return
 
-	position = player.position
+	position = position.move_toward(player.position, 10.0 * delta)
