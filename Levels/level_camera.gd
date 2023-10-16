096759095d9e73e6b3c57e04204e4042d948cf80
karvalho <renato@karvalho.com.br>
2023-10-16 09:34:49 -0300

    Studying camera controller movement bug

--- Levels/level_camera.gd
+++ Levels/level_camera.gd
@@ -1,6 +1,7 @@
 class_name LevelCamera extends Node3D
 
 @export var player: Player
+@export var speed: float = 5.0
 
 @onready var camera: Camera3D = %Camera3D
 
@@ -9,8 +10,11 @@ func _ready() -> void:
 	player.camera_controller = camera
 
 
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	if not player:
 		return
 
-	position = position.move_toward(player.position, 10.0 * delta)
+	position = position.move_toward(player.position, speed * delta)
+	#global_position.x = lerpf(global_position.x, player.global_position.x, speed * delta)
+	#global_position.z = lerpf(global_position.z, player.global_position.z, speed * delta)
+	#print(position)
